5e6e7ab312b4dad4fb81e9f99b3bdf9f4c963f4f
semihvarolymk-png <semihvarol.ymk@gmail.com>
2026-09-19 10:47:07 +0300

    refactor: split game engines into oyunlar modules

---
+++ backend/app/oyunlar/registry.py
@@ -0,0 +1,2 @@
+from . import roulette,cups,horse_race,wheel,vault,crash,blackjack
+GAME_ENGINES={"roulette":roulette,"cups":cups,"horse_race":horse_race,"wheel":wheel,"vault":vault,"crash":crash,"blackjack":blackjack}
